753718980ea4904cc50e2ab10eca5aba289eabbf
Serchinastico <github@serchinastico.com>
2025-04-02 14:19:06 +0200

    refactor: run prettier

--- src/screens/PokemonListScreen.tsx
+++ src/screens/PokemonListScreen.tsx
@@ -1,7 +1,5 @@
 import { View } from "react-native";
 
 export const PokemonListScreen = () => {
-  return (
-    <View className="flex-1 bg-red-500 justify-center items-center"></View>
-  );
+  return <View className="flex-1 bg-red-500 justify-center items-center"></View>;
 };
