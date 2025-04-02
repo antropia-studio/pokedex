db4adaba4339a5b76c2bfe036c7c4de40147f2fe
Serchinastico <github@serchinastico.com>
2025-04-02 11:54:59 +0200

    build: add lint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import jsonFiles from "eslint-plugin-json-files";
+import perfectionist from "eslint-plugin-perfectionist";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  allConfig: eslint.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: eslint.configs.recommended,
+});
+
+const prettierConfig = compat.extends("prettier");
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...prettierConfig,
+  perfectionist.configs["recommended-natural"],
+  {
+    ignores: ["**/node_modules/**/*", "**/android/**/*", "**/ios/**/*", ".commitlintrc.js", "*.config.js"],
+    plugins: { "json-files": jsonFiles, prettier },
+    rules: {
+      "json-files/sort-package-json": "error",
+      "no-empty": "off",
+      "perfectionist/sort-imports": "error",
+      "perfectionist/sort-objects": "error",
+      "prettier/prettier": "error",
+    },
+  }
+);
