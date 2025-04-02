cad68c6eb861dbf116bbdc3d1eb1d2a1e0f64bdf
Serchinastico <github@serchinastico.com>
2025-04-02 13:10:36 +0200

    feat: add safe area provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "style/global.css"; // Load Nativewind styles
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { PokemonListScreen } from "./screens/PokemonListScreen";
 
@@ -12,10 +13,12 @@ const Stack = createNativeStackNavigator<NavigationParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen component={PokemonListScreen} name="pokemonList" />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen component={PokemonListScreen} name="pokemonList" />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
