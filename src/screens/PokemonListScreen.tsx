1c3b26859f23d35cad04e77ed6591b66ab5825e2
Serchinastico <github@serchinastico.com>
2025-04-09 15:22:26 +0200

    feat: implement v4.3

--- src/screens/PokemonListScreen.tsx
+++ src/screens/PokemonListScreen.tsx
@@ -42,29 +42,54 @@ const POKEMON: Pokemon[] = [
   },
 ];
 
+type DescriptionItem = { description: string; type: "description" };
+type FlashListItem = DescriptionItem | PokemonItem;
+type PokemonItem = { pokemon: Pokemon; type: "pokemon" };
+
 export const PokemonListScreen = () => {
   const { bottom } = useSafeAreaInsets();
+
+  const items: FlashListItem[] = [
+    {
+      description: `Explore the complete world of Pokémon at your fingertips. Browse through hundreds of Pokémon, discover their unique attributes, and become the ultimate Pokémon expert. Tap on any Pokémon to reveal detailed information and start your journey!`,
+      type: "description",
+    },
+    ...POKEMON.map((pokemon) => ({ pokemon, type: "pokemon" as const })),
+  ];
 
   return (
     <SafeAreaView className="flex-1" edges={["top", "right", "left"]}>
       <View className="flex-1 flex-col px-6">
         <Text className="text-6xl font-black py-4">Pokedex</Text>
-        <Text className="text-lg">
-          Explore the complete world of Pokémon at your fingertips. Browse through hundreds of Pokémon, discover their
-          unique attributes, and become the ultimate Pokémon expert. Tap on any Pokémon to reveal detailed information
-          and start your journey!
-        </Text>
 
         <FlashList
-          className="pt-10"
+          className="pt-2"
           contentContainerStyle={{ paddingBottom: bottom }}
-          data={POKEMON}
+          data={items}
           numColumns={2}
-          renderItem={({ item }) => (
-            <View className="flex-1 items-center pb-10">
-              <PokemonCard {...item} />
-            </View>
-          )}
+          overrideItemLayout={(layout, item) => {
+            if (item.type === "description") {
+              layout.span = 2;
+            }
+          }}
+          renderItem={({ item }) => {
+            switch (item.type) {
+              case "description":
+                return (
+                  <Text className="text-lg pt-6 pb-10">
+                    Explore the complete world of Pokémon at your fingertips. Browse through hundreds of Pokémon,
+                    discover their unique attributes, and become the ultimate Pokémon expert. Tap on any Pokémon to
+                    reveal detailed information and start your journey!
+                  </Text>
+                );
+              case "pokemon":
+                return (
+                  <View className="flex-1 items-center pb-10">
+                    <PokemonCard {...item.pokemon} />
+                  </View>
+                );
+            }
+          }}
           showsVerticalScrollIndicator={false}
         />
       </View>
